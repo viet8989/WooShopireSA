dc7ba9fb2ac4f0dff7b8b4230c0fcb103138d116
viet8989 <48267581+viet8989@users.noreply.github.com>
2025-09-09 10:02:40 +0000

    set slide imges of product detail

--- wp-content/plugins/woocommerce/templates/single-product/product-image.php
+++ wp-content/plugins/woocommerce/templates/single-product/product-image.php
@@ -37,24 +37,118 @@ $wrapper_classes   = apply_filters(
 		'images',
 	)
 );
+
+// Enqueue Owl Carousel assets
+add_action('wp_enqueue_scripts', function() {
+    if (is_product()) {
+        wp_enqueue_style('owl-carousel', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css');
+        wp_enqueue_style('owl-theme', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css');
+        wp_enqueue_script('owl-carousel', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js', array('jquery'), null, true);
+        wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
+    }
+});
 ?>
-<div class="<?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
-	<div class="woocommerce-product-gallery__wrapper">
-		<?php
-		if ( $post_thumbnail_id ) {
-			$html = wc_get_gallery_image_html( $post_thumbnail_id, true );
-		} else {
-			$wrapper_classname = $product->is_type( ProductType::VARIABLE ) && ! empty( $product->get_available_variations( 'image' ) ) ?
-				'woocommerce-product-gallery__image woocommerce-product-gallery__image--placeholder' :
-				'woocommerce-product-gallery__image--placeholder';
-			$html              = sprintf( '<div class="%s">', esc_attr( $wrapper_classname ) );
-			$html             .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
-			$html             .= '</div>';
-		}
 
-		echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+<div class="<?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>">
+    <figure class="woocommerce-product-gallery__wrapper owl-carousel owl-theme">
+        <?php
+        // Add main image
+        if ($post_thumbnail_id) {
+            $main_image_url = wp_get_attachment_image_url($post_thumbnail_id, 'full');
+            $main_image_alt = get_post_meta($post_thumbnail_id, '_wp_attachment_image_alt', true);
+            ?>
+            <div class="woocommerce-product-gallery__image" data-thumb="<?php echo esc_url($main_image_url); ?>">
+                <a href="<?php echo esc_url($main_image_url); ?>">
+                    <img src="<?php echo esc_url($main_image_url); ?>" 
+                         class="wp-post-image" 
+                         alt="<?php echo esc_attr($main_image_alt); ?>" 
+                         loading="lazy">
+                </a>
+            </div>
+            <?php
+        }
 
-		do_action( 'woocommerce_product_thumbnails' );
-		?>
-	</div>
+        // Add gallery images
+        $attachment_ids = $product->get_gallery_image_ids();
+        if ($attachment_ids) {
+            foreach ($attachment_ids as $attachment_id) {
+                $image_url = wp_get_attachment_image_url($attachment_id, 'full');
+                $image_alt = get_post_meta($attachment_id, '_wp_attachment_image_alt', true);
+                ?>
+                <div class="woocommerce-product-gallery__image" data-thumb="<?php echo esc_url($image_url); ?>">
+                    <a href="<?php echo esc_url($image_url); ?>">
+                        <img src="<?php echo esc_url($image_url); ?>" 
+                             class="wp-post-image" 
+                             alt="<?php echo esc_attr($image_alt); ?>" 
+                             loading="lazy">
+                    </a>
+                </div>
+                <?php
+            }
+        }
+        ?>
+    </figure>
+
+    <ol class="product-control-nav owl-carousel owl-theme">
+        <?php
+        // Thumbnails for main image and gallery
+        if ($post_thumbnail_id) {
+            $thumb_url = wp_get_attachment_image_url($post_thumbnail_id, 'thumbnail');
+            echo '<li><img src="' . esc_url($thumb_url) . '" alt="Thumbnail"></li>';
+        }
+        if ($attachment_ids) {
+            foreach ($attachment_ids as $attachment_id) {
+                $thumb_url = wp_get_attachment_image_url($attachment_id, 'thumbnail');
+                echo '<li><img src="' . esc_url($thumb_url) . '" alt="Thumbnail"></li>';
+            }
+        }
+        ?>
+    </ol>
+
+    <script type="text/javascript">
+    jQuery(document).ready(function($) {
+        // Initialize main carousel
+        var $mainCarousel = $('.woocommerce-product-gallery__wrapper');
+        var $thumbCarousel = $('.product-control-nav');
+
+        $mainCarousel.owlCarousel({
+            items: 1,
+            nav: true,
+            dots: false,
+            navText: [
+                '<i class="fas fa-angle-left"></i>',
+                '<i class="fas fa-angle-right"></i>'
+            ]
+        });
+
+        // Initialize thumbnail carousel
+        $thumbCarousel.owlCarousel({
+            items: 4,
+            nav: true,
+            dots: false,
+            margin: 20,
+            navText: ['‹', '›']
+        });
+
+        // Sync thumbnails with main carousel
+        $thumbCarousel.on('click', 'li', function(e) {
+            e.preventDefault();
+            var index = $(this).parent().index();
+            $mainCarousel.trigger('to.owl.carousel', [index, 300, true]);
+        });
+
+        $mainCarousel.on('changed.owl.carousel', function(e) {
+            var index = e.item.index;
+            $thumbCarousel.trigger('to.owl.carousel', [index, 300, true]);
+            $thumbCarousel.find('.owl-item').removeClass('current');
+            $thumbCarousel.find('.owl-item').eq(index).addClass('current');
+        });
+
+        setTimeout(() => {
+            if(jQuery('.owl-nav').length > 1) {
+                jQuery('.owl-nav').eq(1).hide();
+            }
+        }, 1000);
+    });
+    </script>    
 </div>
